eaf02340a1bed858b3366e33a0cdfc955d3a1c47
NiceNewton <nicenewton1512@gmail.com>
2025-04-03 09:20:24 +0530

    LAST CHANGE

--- frontend/src/pages/lecture/Lecture.jsx
+++ frontend/src/pages/lecture/Lecture.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { server } from '../../main';
 import Loading from '../../components/loading/Loading';
 import './lecture.css';
+import toast from 'react-hot-toast';
 
 const Lecture = ({ user }) => {
     const [lectures, setLectures] = useState([]);
@@ -12,6 +13,16 @@ const Lecture = ({ user }) => {
     const [lecLoading, setLecLoading] = useState(false);
     const [show, setShow] = useState(false);
     const params = useParams();
+    const navigate = useNavigate();
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [video, setVideo] = useState('');
+    const [videoPreview, setVideoPreview] = useState('');
+    const [btnLoading, setBtnLoading] = useState(false);
+
+    if (user && user.role !== 'admin'&& !user.subscription.includes(params.id)) {
+        return navigate('/');
+    }
 
     async function fetchLectures() {
         try {
@@ -37,6 +48,58 @@ const Lecture = ({ user }) => {
             console.error(error);
         } finally {
             setLecLoading(false);
+        }
+    }
+
+    const changeVideoHandler = (e) => {
+        const file = e.target.files[0];
+        const reader = new FileReader();
+
+        reader.readAsDataURL (file);
+
+        reader.onloadend = () => {
+            setVideoPreview(reader.result)
+            setVideo(file);
+        }
+    }
+
+    const submitHandler = async (e) => {
+        setBtnLoading(true);
+        e.preventDefault();
+        const myForm = new FormData();
+
+        myForm.append('title', title);
+        myForm.append('description', description);
+        myForm.append('file', video);
+
+        try {
+            const { data } = await axios.post(`${server}/api/course/${params.id}`, myForm, {   
+                headers: {
+                    token: localStorage.getItem('token'),
+                },
+            });
+            toast.success(data.message);
+            setBtnLoading(false);
+            setShow(false);
+            fetchLectures();
+        } catch (error) {
+            toast.error(error.response.data.message)
+            setBtnLoading(false);
+        }
+    };
+
+    const deleteHandler = async (id) => {
+        if (confirm('Are you sure you want to delete this lecture?')) {
+           try {
+            const { data } = await axios.delete(`${server}/api/lecture/${id}`, {
+                headers: { token: localStorage.getItem('token') },
+            });
+            toast.success(data.message);
+            fetchLectures();
+
+           } catch (error) {
+            toast.error(error.response.data.message);
+           }
         }
     }
 
@@ -81,17 +144,24 @@ const Lecture = ({ user }) => {
                         {show && (
                             <div className="lecture-form">
                                 <h2>Add Lecture</h2>
-                                <form>
+                                <form onSubmit={submitHandler} >
                                     <label htmlFor="text">Title</label>
-                                    <input type="text" required />
+                                    <input type="text" 
+                                    value={title} onChange = {e=>setTitle(e.target.value)}
+                                     required />
 
                                     <label htmlFor="text">Description</label>
-                                    <input type="text" required />
+                                    <input type="text" 
+                                    value={description} onChange = {e=>setDescription(e.target.value)} 
+                                     required />
 
-                                    <input type="file" placeholder="Choose video" required />
+                                    <input type="file" placeholder="Choose video" onChange={changeVideoHandler} required />
 
-                                    <button type="submit" className="common-btn">
-                                        Add Lecture
+                                    {
+                                     videoPreview && <video src={videoPreview} alt="" width = {300} controls></video>}
+
+                                    <button disabled ={btnLoading} type="submit" className="common-btn">
+                                        {btnLoading ? 'Uploading...' : 'Add Lecture'}
                                     </button>
                                 </form>
                             </div>
@@ -106,7 +176,9 @@ const Lecture = ({ user }) => {
                                         {i + 1}. {e.title}
                                     </div>
                                     {user && user.role === 'admin' && (
-                                        <button className="common-btn" style={{ background: 'red' }}>
+                                        <button className="common-btn" style={{ background: 'red' }}
+                                        onClick={()=>deleteHandler(e._id)}
+                                        >
                                             Delete {e.title}
                                         </button>
                                     )}
